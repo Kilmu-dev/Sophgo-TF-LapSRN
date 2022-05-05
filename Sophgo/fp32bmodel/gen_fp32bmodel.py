a930f2894110bb1085f729085aa0bf5091338774
Kilmu <fuchao.wang@sophgo.com>
2022-05-05 13:47:39 +0800

    生成fp32bmodel

--- Sophgo/fp32bmodel/gen_fp32bmodel.py
+++ Sophgo/fp32bmodel/gen_fp32bmodel.py
@@ -8,7 +8,7 @@ bmnett.compile(
     net_name = "LapSRN",              ## Necessary
     input_names=["IteratorGetNext"],    ## Necessary, when .h5 use None
     output_names=["NCHW_output"], ## Necessary, when .h5 use None
-    opt = 2,                           ## optional, if not set, default equal to 1
+    opt = 1,                           ## optional, if not set, default equal to 1
     dyn = False,                       ## optional, if not set, default equal to False
     cmp = True,                        ## optional, if not set, default equal to True
     enable_profile = True              ## optional, if not set, default equal to False
