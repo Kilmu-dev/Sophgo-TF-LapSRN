749bbeb3922b73c79355aef9dc79b4acaf644b1c
Kilmu <fuchao.wang@sophgo.com>
2022-05-05 13:34:15 +0800

    生成fp32bmodel

--- Sophgo/fp32bmodel/gen_fp32bmodel.py
+++ Sophgo/fp32bmodel/gen_fp32bmodel.py
@@ -4,7 +4,7 @@ bmnett.compile(
     model = "/workspace/work/lapsrn/Sophgo-TF-LapSRN/export/LapSRN_x8.pb",     ## Necessary
     outdir = "/fp32bmodel",                    ## Necessary
     target = "BM1684",                ## Necessary
-    shapes = [[3,3,1,64]],     ## Necessary
+    shapes = [[3,3,1,64],[4,4,1,4]],     ## Necessary
     net_name = "LapSRN",              ## Necessary
     input_names=["IteratorGetNext:0"],    ## Necessary, when .h5 use None
     output_names=["NCHW_output:0"], ## Necessary, when .h5 use None
